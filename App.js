215a34415d37886aedd0144f27ad7b51e3ca1ea3
Pratikpalande <126753222+Pratikpalande@users.noreply.github.com>
2023-04-05 23:01:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+  
+  {/* <Navbar /> */}
+  <Navbar title="Textutils" about="About Us " /> 
+    </>
+  );
+}
+
+export default App;
